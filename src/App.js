fb6dee6851b245349ca87649e5f9a57f70b0bb3e
AbhaySingh502 <abhaysingh65534@gmail.com>
2024-01-03 16:47:44 +0530

    new files were added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './components/Home';
 import Clubs_card from './clubs/Clubs_card';
 import Kritash from './clubspages/Kritash';
+import Abhivayakt from './clubspages/Abhivayakt';
+import Arthitics from './clubspages/Arthitics';
+import Astronomy from './clubspages/Astronomy';
+import Coding from './clubspages/Coding';
+import Dance from './clubspages/Dance';
+import Dramatizer from './clubspages/Dramatizer';
+import Malang from './clubspages/Malang';
+import Robotics from './clubspages/Robotics';
+import Sae from './clubspages/Sae';
+import Wellbeing from './clubspages/Wellbeing';
 import Sport_main from './sports/Sport_main';
 function App() {
   return (
@@ -13,10 +23,20 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Clubs" element={<Clubs_card />} />
-          {/* <Route path="/Clubpage" element={<Indiviual_page />} /> */}
-          <Route path="/Kritash" element={<Kritash />} />
           <Route path="/Sports" element={<Sport_main />} />
-
+        </Routes>
+        <Routes>
+          <Route path="/Abhivayakt" element={<Abhivayakt />} />
+          <Route path="/Arthitics" element={<Arthitics />} />
+          <Route path="/Astronomy" element={<Astronomy />} />
+          <Route path="/Coding" element={<Coding />} />
+          <Route path="/Dance" element={<Dance />} />
+          <Route path="/Dramatizer" element={<Dramatizer />} />
+          <Route path="/Kritash" element={<Kritash />} />
+          <Route path="/Malang" element={<Malang />} />
+          <Route path="/Robotics" element={<Robotics />} />
+          <Route path="/Sae" element={<Sae />} />
+          <Route path="/Wellbeing" element={<Wellbeing />} />
         </Routes>
       </BrowserRouter>
 
---
+++ src/data/coding_data.js
@@ -0,0 +1,34 @@
+const coding_data = [
+    {
+        key: 1,
+        name: "Blood Donation Drive",
+        para: "Kritash in collaboration with Medical Centre, IIT JAMMU and GMC Jammu hosted a blood donation drive on Oct 13, 2023. In this donation drive more than 50 volunteers donated blood for a good cause, making it a successful event.",
+        source_url: "#",
+    },
+    {
+        key: 2,
+        name: "Cleanliness Drive",
+        para: "Kritash led a transformative cleanliness initiative near IIT Jammu on 1st of October, 2023, uniting students, faculty, and staff to enhance the campus environment. The campaign included a thorough litter clean-up effort where enthusiastic volunteers used trash bags to revitalize neglected areas. This drive not only improved aesthetics but also promoted environmental stewardship, fostering a more responsible and cleaner community",
+        source_url: "#"
+    },
+    {
+        key: 3,
+        name: "Guest talk on Waste Management",
+        para: "Kritash hosted a guest talk on the topic Solid waste management,on 18th of October 2023, where Miss Nupur Tandon ( Founding Director, Pro Waste Concepts Pvt. Ltd.) was the guest speaker. She shared her knowledge and guided us through various methods through which we could reduce and manage waste properly.",
+        source_url: "#"
+    },
+    {
+        key: 4,
+        name: "Free Multispeciality Health Checkup camp",
+        para: "Kritash, in partnership with SIBA, organized a free multispecialty health checkup for the welfare of IITians on September 22, 2023. Amandeep Hospital, Green Court Hospital, and Sehora Dental College participated. The event was inaugurated by Prof. Vijayan Palippattu and Dr. Karunika. Students, faculty, staff, and residents received free checkups, boosting doctor's morale. Services included eye tests, dental care, and more. The event concluded with acknowledgments for those who contributed to its success.",
+        source_url: "#"
+    },
+    {
+        key: 5,
+        name: "Aksharshala Teaching Program",
+        para: "Weekends see Kritash members venturing to the nearby Kamini village,where they impart knowledge to underprivileged children, embodying the belief that education is not confined to classrooms but extends to every corner of society",
+        source_url: "#"
+    },
+]
+
+export default coding_data
